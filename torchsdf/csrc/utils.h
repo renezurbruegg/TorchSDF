2c1e8c6362ebf74603464e64a6ead4d911014933
zrene <zrene@ethz.ch>
2025-09-19 16:53:58 +0200

    coderelease

--- torchsdf/csrc/utils.h
+++ torchsdf/csrc/utils.h
@@ -23,7 +23,7 @@
   /* Code block avoids redefinition of cudaError_t error */ \
   do { \
     cudaError_t error = condition; \
-    CHECK_EQ(error, cudaSuccess) << " " << cudaGetErrorString(error); \
+    TORCH_CHECK_EQ(error, cudaSuccess) << " " << cudaGetErrorString(error); \
   } while (0)
 
 #define PRIVATE_CASE_TYPE(ENUM_TYPE, TYPE, TYPE_NAME, ...) \
